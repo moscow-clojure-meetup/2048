846716f0c2c7700a3ffc54578f1e57c5bc9a659b
Andrey Neverov <neverov.and@gmail.com>
2018-09-22 19:01:53 +0300

    Pretty ascii number.

--- src/dojo2048/letters.clj
+++ src/dojo2048/letters.clj
@@ -1,6 +1,10 @@
 (ns dojo2048.letters)
 
-(defn long-str [& strings] (clojure.string/join "\n" strings))
+(defn long-str [& strings] strings)
+
+(def zero (long-str " _ "
+                    "/ \\"
+                    "\\_/"))
 
 (def one (long-str "   "
                    "/| "
@@ -18,42 +22,55 @@
                     "|_|" 
                     "  |"))
 
-
 (def five (long-str " _ "
                     "|_ "
                     " _)"))
-
 
 (def six (long-str " _ "
                    "|_ "
                    "|_)"))
 
-
 (def seven (long-str "__ "
                      " / "
                      "/  "))
-
 
 (def eight (long-str " _ "
                      "(_)"
                      "(_)"))
 
-
 (def nine (long-str " _ "
                     "(_|"
                     "  |"))
 
+(def e (long-str "   " "   " "   "))
+
 (defn convert-digit [digit]
-  (case digit
-    "1" one
-    "2" two
-    "3" three
-    "4" four
-    "5" five
-    "6" six
-    "7" seven
-    "8" eight
-    "9" nine))
+  (case (read-string (str digit))
+    0 zero
+    1 one
+    2 two
+    3 three
+    4 four
+    5 five
+    6 six
+    7 seven
+    8 eight
+    9 nine
+    e))
  
 (defn convert-number [number]
-  ((str number) map convert-digit))
+  (let [normalized (case (count (str number))
+                      1 (str "!!!" number)
+                      2 (str "!!" number)
+                      3 (str "!" number)
+                      4 (str number))]
+    (map convert-digit normalized)))
+ 
+(defn print-number [number]
+  (for [i (range 3)]
+    (do
+      (doall 
+        (for [j (range (count number))]
+          (print (nth (nth number j) i)))) 
+      (println ""))))
+
