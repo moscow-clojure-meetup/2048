9cb0b6da0132fc3e3133a00a2375afb5d511c163
Alexander Ponimaskin <ponimaskin@gmail.com>
2018-09-22 16:56:33 +0300

    init board

--- src/dojo2048/core.clj
+++ src/dojo2048/core.clj
@@ -1,6 +1,13 @@
 (ns dojo2048.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn init-board []
+  (let [board (make-array Long/TYPE 4 4)
+        [x1 y1 x2 y2]
+        (repeatedly 4 #(rand-int 4))]
+    (if (= [x1 y1] [x2 y2])
+      (init-board)
+      (do
+        (aset board x1 y1 2)
+        (aset board x2 y2 2)
+        board))))
+
